65680a1d10c2e46f34aedb7fa968773210617a84
Jake M <jakemaskiewicz@gmail.com>
2013-12-19 22:25:18 -0800

    added basic window code and linked SFML

--- ping-wars/main.cpp
+++ ping-wars/main.cpp
@@ -4,6 +4,23 @@
 
 
 int main(char* args[]) {
-	//TODO
-	std::cout << "Hello, World" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Ping Wars");
+
+	while (window.isOpen()) {
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color::Black);
+
+		//TODO - draw everything here
+
+		window.display();
+	}
+
+	
+
 }
